b6a8a73336475e406cebe0091198382074067e0f
DodoSeal <maxhmcd08@gmail.com>
2026-06-01 23:42:57 -0600

    Feat: Begin learning Svelte - Extremely basic navigation implemented

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-screen h-screen flex flex-col items-center place-content-center">
+    <h1 class="text-white text-center font-bold text-3xl">Dashboard Page!</h1>
+    <a class="text-blue-500 hover:text-blue-400 hover:underline cursor-pointer transition-all duration-200 text-center font-bold text-xl m-4" href="/design">Design?</a>
+    <a class="text-blue-500 hover:text-blue-400 hover:underline cursor-pointer transition-all duration-200 text-center font-bold text-xl m-4" href="/">Back?</a>
+</div>
---
+++ src/routes/design/+page.svelte
@@ -0,0 +1,4 @@
+<div class="w-screen h-screen flex flex-col items-center place-content-center">
+    <h1 class="text-white text-center font-bold text-3xl">Design Page!</h1>
+    <a class="text-blue-500 hover:text-blue-400 hover:underline cursor-pointer transition-all duration-200 text-center font-bold text-xl m-4" href="/">Back?</a>
+</div>
