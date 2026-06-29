1223f95d5dac9f04bee0b4e4feb29d02d86f03b0
DodoSeal <maxhmcd08@gmail.com>
2026-06-28 23:31:17 -0600

    Fix: Copyright component is fixed in place

--- src/routes/components/Copyright.svelte
+++ src/routes/components/Copyright.svelte
@@ -9,6 +9,6 @@
     });
 </script>
 
-<div class="w-full h-6 absolute flex items-center place-content-center bottom-0 mb-1">
+<div class="w-full h-6 fixed flex items-center place-content-center bottom-0 mb-1">
     <p id="copyright" class="font-thin">© 2026 Max McDaniel</p>
 </div>
